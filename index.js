6c116e297b802afde22ea56eb2f4db595131f458
Zachary King <lax0248917@gmail.com>
2020-01-18 16:35:37 -0500

    Application version 1.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
 const slackToken = require('./module');
+const apiKey = require('./module');
 
 const bot = new SlackBot({
 	token : slackToken.slack_token,
@@ -8,11 +9,11 @@ const bot = new SlackBot({
 });
 
 bot.on('start', () => {
-	const params = {
+	const icon = {
 		icon_emoji : ':sunny:'
 	};
 
-	bot.postMessageToChannel('general', 'Get your local Weather, with @WeatherBot!', params);
+	bot.postMessageToChannel('general', 'Get your local Weather, with @WeatherBot!', icon);
 });
 
 // Error Handler
@@ -34,14 +35,22 @@ function handleMessage (message) {
 }
 
 // Get Local Weather
-function localWeather () {
-	axios.get('http://api.icndb.com/jokes/random').then((res) => {
-		const joke = res.data.value.joke;
 
-		const params = {
+const params = {
+	access_key : apiKey.api_key,
+	query : 'Saint Joseph',
+	units : 'f',
+};
+
+function localWeather () {
+	axios.get('http://api.weatherstack.com/current', {params}).then((res) => {
+		const currentTemp = res.data.current.temperature;
+		const weatherCondition = res.data.current.weather_descriptions
+
+		const icon = {
 			icon_emoji : ':partly_sunny:'
 		};
 
-		bot.postMessageToChannel('general', `Weather Forecast: ${joke}`, params);
+		bot.postMessageToChannel('general', `Current Forecast for Saint Joseph, Michigan is: ${weatherCondition} and ${currentTemp}`, icon);
 	});
 }
